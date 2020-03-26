0e1875dbf18133dfc40d40d9c77b65d97e89dd01
johnflentri <jpflentri@gmail.com>
2020-03-26 17:19:37 +0100

    Data models added

---
+++ location/model.js
@@ -0,0 +1,19 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Location = db.define("location", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+  lat: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  lng: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  }
+})
+
+module.exports = Location;
